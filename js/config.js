0098c8501ae094fb12fa62dd301c9e3b951f6fcb
eswar585 <eswaravuthu04@gmail.com>
2026-08-18 18:52:59 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL =
+window.SUPABASE_URL =
   "https://grmgrvlcgdjgeqnwwvos.supabase.co";
 
-const SUPABASE_ANON_KEY =
+window.SUPABASE_ANON_KEY =
   "sb_publishable_6Wy84w4Gw6kRw7UvDIo1Rg_nXJmp73f";
